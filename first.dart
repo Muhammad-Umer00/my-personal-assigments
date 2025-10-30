d8d43c1d97d6bb75989dab95815ddea1c6e196b7
Muhammad Umer-00 <us8012137@gmail.com>
2025-10-30 05:29:24 -0700

    my first assigment it is a atm project by sir saad

---
+++ first.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+void main() {
+  String withDraw = "1- withdraw";
+  String showBAlance = "2- ShowBalance";
+  String Deposite = "3- Deposite";
+  String Exit = "4- Exit";
+  double balance = 4000;
+  print("Umer ATM");
+
+  bool exit = true;
+  while (exit == true) {
+    print(withDraw);
+    print(showBAlance);
+    print(Deposite);
+    print(Exit);
+    String? input = stdin.readLineSync();
+    int option = int.parse(input!);
+    if (option <= 4) {
+      switch (option) {
+        case 1:
+          print("How much do you want to withdraw");
+          String? input = stdin.readLineSync();
+          int value = int.parse(input!);
+          if (value <= balance) {
+            balance -= value;
+            print(balance);
+          } else {
+            print("you are out of balance");
+          }
+          break;
+
+        case 2:
+          print(balance);
+
+          break;
+        case 3:
+          print("How much do you want to deposite");
+          String? input = stdin.readLineSync();
+          int value = int.parse(input!);
+          if (value >= 0) {
+            balance += value;
+            print(balance);
+          } else {
+            print("please enter a amount");
+          }
+          break;
+        case 4:
+          exit = false;
+          print("visit again maalik");
+        default:
+          print("please choose correct option");
+      }
+    } else {
+      print("please choose correct option");
+    }
+  }
+}
